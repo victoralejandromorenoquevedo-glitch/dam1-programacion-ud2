60a7228b7024118d8a94ee51ca638d0eb166e373
victoralejandromorenoquevedo-glitch <victoralejandromorenoquevedo@gmail.com>
2025-10-01 14:17:00 +0200

    Commit

---
+++ src/utilidades/DatosUsaurio.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import utilidades.ProcesadorTexto;
+public class DatosUsaurio {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Introduce tu nombre: ");
+    String = sc.nextLine();
+    System.out.println("Introduce tu edad: ");
+    int = sc.nextLine();
+    System.out.println("Introduce tu correo electrónico: ");
+    int = sc.nextLine();
+
+    }
+}
